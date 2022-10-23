b064a5c440b2c629c382cd880bef3a33b6ac7767
Dongting Cai <cai00151@morris.umn.edu>
2022-10-23 17:08:14 -0500

    Update `summarize_tree.sh`, Fix PTNL Case Error
    < Issue #4 >
    Update `summarize_tree.sh`, Fix a potential error that might lead to the test failure.

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -6,4 +6,4 @@ countDir="$(find "$inputDir" -type d | wc -l | xargs)"
 countFile="$(find "$inputDir" -not -type d | wc -l | xargs)"
 
 echo "There were $countDir directories."
-echo "There were $countFile regular Files."
+echo "There were $countFile regular files."
