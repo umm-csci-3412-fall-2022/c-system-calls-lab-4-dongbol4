909f6349b9a96945a5ead6d3efaa64f2d4c4df3f
Dongting Cai <cai00151@morris.umn.edu>
2022-10-22 21:48:48 -0500

    Update `summarize_tree.sh`, Fix missing quote
    < Issue #4 >
    Update `summarize_tree.sh`, Fix missing quote

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -2,8 +2,8 @@
 
 inputDir="$1"
 
-countDir=$(find "$inputDir" -type d | wc -l | xargs)
-countFile=$(find "$inputDir" -type f | wc -l | xargs)
+countDir="$(find "$inputDir" -type d | wc -l | xargs)"
+countFile="$(find "$inputDir" -not -type d | wc -l | xargs)"
 
 echo "There were $countDir directories."
 echo "There were $countFile regular Files."
