279885596fb8d5489c6d61a78042fe9718b610b6
Dongting Cai <cai00151@morris.umn.edu>
2022-10-22 21:21:51 -0500

    Update `summarize_tree.sh`, Finish Issue #4
    < Issue #4 >
    Finish Issue #4, which uses `find` to do the traversal, and let `wc` do the counting by using shell.

---
+++ summarize_tree/summarize_tree.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+inputDir = "$1"
+
+countDir = $( find "$inputDir" -type d | wc -l | xargs )
+countFile = $( find "$inputDir" -not -type d | wc -l | xargs )
+
+echo "There were $countDir directories."
+echo "There were $countFile regular Files."
