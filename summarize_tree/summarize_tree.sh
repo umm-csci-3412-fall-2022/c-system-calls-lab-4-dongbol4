2e8875c50fee734827da95e200df1c8f04a77901
Dongting Cai <cai00151@morris.umn.edu>
2022-10-22 21:31:05 -0500

    Update `summarize_tree.sh`, Fix type issue
    < Issue #4 >
    Update `summarize_tree.sh`, Fix type issue

--- summarize_tree/summarize_tree.sh
+++ summarize_tree/summarize_tree.sh
@@ -3,7 +3,7 @@
 inputDir="$1"
 
 countDir=$(find "$inputDir" -type d | wc -l | xargs)
-countFile=$(find "$inputDir" -not -type d | wc -l | xargs)
+countFile=$(find "$inputDir" -type f | wc -l | xargs)
 
 echo "There were $countDir directories."
 echo "There were $countFile regular Files."
